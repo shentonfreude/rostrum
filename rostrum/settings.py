738a406968e9e7726ec2b66b68d7115075fb55dd
Chris Shenton <chris@koansys.com>
2012-06-20 10:03:16 -0400

    Flatten hierarchy, got views working with images, bootstrap

--- rostrum/settings.py
+++ rostrum/settings.py
@@ -110,10 +110,11 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'rostrum.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'rostrum.wsgi.application'
+#WSGI_APPLICATION = 'rostrum.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -135,8 +136,8 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     # Our app: (FOR NOW, need rostrum.app for syncdb, 'app' for import
         #
-    'rostrum.app',
-    #'app',
+    #'rostrum.app',
+    'app',
     'bootstrapform',
 )
 
